02b9ef8f739bb588748aea69e868c8c611d6c9af
v02qabs <takesue090@gmail.com>
2025-03-06 02:51:33 +0900

    add command

---
+++ command/command.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+
+public class ExternalCommand
+{
+    public static void Main(string[] args)
+    {
+        try
+        {
+            Process process = new Process();
+            process.StartInfo.FileName = "cmd.exe";
+            process.StartInfo.Arguments = "/c dir"; // 現在のディレクトリの内容を表示
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.RedirectStandardError = true;
+            process.StartInfo.UseShellExecute = false;
+
+            process.Start();
+            string output = process.StandardOutput.ReadToEnd();
+            string error = process.StandardError.ReadToEnd();
+            process.WaitForExit();
+
+            Console.WriteLine("出力:");
+            Console.WriteLine(output);
+
+            if (!string.IsNullOrEmpty(error))
+            {
+                Console.WriteLine("エラー:");
+                Console.WriteLine(error);
+            }
+
+            Console.WriteLine("終了コード: " + process.ExitCode);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("エラーが発生しました: " + ex.Message);
+        }
+    }
+}
