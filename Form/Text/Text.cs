adcc93ede9236eb8e87dc2da0b3832e975b78ef0
v02qabs <takesue090@gmail.com>
2025-03-14 01:07:43 +0900

    add count

--- Form/Text/Text.cs
+++ Form/Text/Text.cs
@@ -1,17 +1,34 @@
 using System;
+using System.Text;
 using System.Windows.Forms;
+using System.Drawing;
 
+class Hello : Form {
+    private Label count_up_label; // メンバ変数として宣言
 
-class Hello : Form{
-	
-	public Hello(){
-		TextBox box = new TextBox();
-		this.Controls.Add(box);
-	}
-	
+    public Hello() {
+        count_up_label = new Label(); // this.count_up_label に代入
+        count_up_label.Location = new Point(10, 10);
+        count_up_label.Text = "0";
+        this.Controls.Add(count_up_label);
 
+        Button download_button = new Button();
+        download_button.Location = new Point(200, 30);
+        download_button.Text = "download"; // タイポ修正
+        this.Controls.Add(download_button);
 
-	public static void Main(){
-		Application.Run(new Hello());
-	}
+        // イベントハンドラ名修正
+        download_button.Click += new EventHandler(DownloadButton_Click);
+    }
+
+    public static void Main() {
+        Application.Run(new Hello());
+    }
+
+    // イベントハンドラ名を修正
+    private void DownloadButton_Click(object sender, EventArgs e) {
+        int currentValue = Convert.ToInt32(count_up_label.Text);
+        count_up_label.Text = (currentValue + 1).ToString();
+    }
 }
+
