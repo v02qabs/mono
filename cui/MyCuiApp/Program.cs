f1c214ffb35ac1cc5eb94e13bfe8e08bcbbebc6c
v02qabs <takesue090@gmail.com>
2025-10-01 22:12:01 +0900

    git:cui:mono

---
+++ cui/MyCuiApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, CUI World!");
+
+        Console.Write("Your name? > ");
+        string name = Console.ReadLine();
+
+        Console.WriteLine($"Mr.{name} .");
+    }
+}
